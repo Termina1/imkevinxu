e19155e8f2b4297d1fa3ce9547d12a0bd4264c62
Kevin Xu <kevin@imkevinxu.com>
2012-10-23 23:53:30 -0700

    adding xkcd graphs

---
+++ xkcd/index.php
@@ -0,0 +1,165 @@
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>XKCD Graphs in D3</title>
+
+    <style>
+        @font-face {
+            font-family: "xkcd";
+            src: url('http://antiyawn.com/uploads/Humor-Sans.ttf');
+        }
+
+        body {
+            font-family: "xkcd", sans-serif;
+            font-size: 16px;
+            color: #333;
+            text-align: center;
+        }
+
+        a {
+            text-decoration: none;
+            color: steelblue;
+        }
+
+        a:hover {
+            color: #0E5C9E;
+        }
+
+        .container {
+            width: 700px;
+            margin: 20px auto;
+        }
+
+        #plot {
+            width: 700px;
+            margin: 70px auto;
+        }
+
+        #plot h1:nth-child(n+2) {
+            margin-top: 120px;
+        }
+
+        p {
+            margin: 10px 0 20px;
+            font-size: 20px;
+            line-height: 1.5em;
+        }
+
+        text.title {
+            font-size: 20px;
+        }
+
+        path {
+            fill: none;
+            stroke-width: 2.5px;
+            stroke-linecap: round;
+            stroke-linejoin: round;
+        }
+
+        path.axis {
+            stroke: black;
+        }
+
+        path.bgline {
+            stroke: white;
+            stroke-width: 6px;
+        }
+    </style>
+
+    <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-25074402-1']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
+
+</head>
+<body>
+
+    <div class="container">
+
+        <h1>XKCD-style Graphs</h1>
+        <h2>Credit to <a href="http://dan.iel.fm/xkcd/" target="_blank">Dan Foreman-Mackey</a></h2>
+
+    </div>
+
+    <div id="plot"></div>
+
+    <footer class="container">
+
+        <a href="https://twitter.com/charlierguo" target="_blank">@charlierguo</a>
+        &nbsp;
+        <a href="http://twitter.com/imkevinxu" target="_blank">@imkevinxu</a>
+
+    </footer>
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+    <script src="http://d3js.org/d3.v2.min.js"></script>
+    <script src="xkcd.js"></script>
+
+    <script>
+        function f1 (x) {
+            return Math.exp(-0.5 * (x - 1) * (x - 1)) * Math.sin(x + 0.2) + 0.05;
+        }
+        function f2 (x) {
+            return 0.5 * Math.cos(x - 0.5) + 0.1;
+        }
+        var xmin = -1.0,
+            xmax = 7,
+            N = 100,
+            xlim = [xmin - (xmax - xmin) / 16, xmax + (xmax - xmin) / 16],
+            data = d3.range(xmin, xmax, (xmax - xmin) / N).map(function (d) {
+                return {x: d, y: f1(d)};
+            }),
+            data2 = d3.range(xmin, xmax, (xmax - xmin) / N).map(function (d) {
+                return {x: d, y: f2(d)};
+            }),
+            parameters = {  title: "Example",
+                            xlabel: "Yumminess",
+                            ylabel: "Velociraptor Speed",
+                            xlim: xlim },
+            plot = xkcdplot();
+        plot("#plot", parameters);
+        plot.plot(data);
+        plot.plot(data2, {stroke: "red"});
+        plot.draw();
+
+
+
+        function f3 (x) {
+            return Math.cos(x);
+        }
+        function f4 (x) {
+            return Math.sin(x);
+        }
+        var xmin = -1.0,
+            xmax = 14,
+            N = 100,
+            xlim = [xmin - (xmax - xmin) / 16, xmax + (xmax - xmin) / 16],
+            data = d3.range(xmin, xmax, (xmax - xmin) / N).map(function (d) {
+                return {x: d, y: f3(d)};
+            }),
+            data2 = d3.range(xmin, xmax, (xmax - xmin) / N).map(function (d) {
+                return {x: d, y: f4(d)};
+            }),
+            parameters = {  title: "Sin and Cosine",
+                            xlabel: "X",
+                            ylabel: "Y",
+                            xlim: xlim,
+                            ylim: [-1.1, 1.1] },
+            plot = xkcdplot();
+        plot("#plot", parameters);
+        plot.plot(data);
+        plot.plot(data2, {stroke: "red"});
+        plot.draw();
+
+    </script>
+
+</body>
+</html>
