67d2f84761de46f4600d91415054b7cc212dd25f
Kevin Xu <kevin@imkevinxu.com>
2012-10-24 00:46:41 -0700

    adding github ribbon

--- xkcd/index.php
+++ xkcd/index.php
@@ -62,6 +62,8 @@
 
     </footer>
 
+    <a href="https://github.com/imkevinxu/imkevinxu/tree/master/xkcd"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a>
+
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
     <script src="http://d3js.org/d3.v2.min.js"></script>
     <script src="xkcd.js"></script>
